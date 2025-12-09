399df7f7c50d137f9ccbdd4907b5a04ce1c5f542
finastitri-dev <finastitri@std.isi-ska.ac.id>
2025-12-09 16:26:25 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// IMPORT TAILWIND CSS
+import './index.css';
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -7,8 +11,7 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>

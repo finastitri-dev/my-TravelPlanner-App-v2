cd71aac4a9adddced4419acdd43fff4b3a2b3710
finastitri-dev <finastitri@std.isi-ska.ac.id>
2025-12-11 16:22:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./",   // WAJIB: biar CSS & assets ketemu di Vercel
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
